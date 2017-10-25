02f0a6778d910ad2af3c37e9846f037589a9025c
Rik Lewis <rik@rik.onl>
2017-10-25 21:26:47 +0100

    First version ready for release and publish on npm

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 Description
 ===========
 
-Scan PHP files in Gulp and process script and stylesheet tags to add SRI attributes.
+Scan PHP files in Gulp and process script and stylesheet tags to add Sub-Resource Integrity (SRI) attributes.
 
 I tried other plugins, but found the following issues...
 * [gulp-sri-hash](https://www.npmjs.com/package/gulp-sri-hash) - uses Cheerio, which expects HTML and fails on PHP tags
 * [gulp-srizer](https://www.npmjs.com/package/gulp-srizer) - requires OpenSSL and doesn't work on Windows (uses 'cat')
 * [gulp-sri](https://www.npmjs.com/package/gulp-sri) - creates a JSON file with hashes but doesn't update the files inline
 
-For these reasons, I decided to write my own.
+For these reasons, I decided to write my own.  
+
+This plugin doesn't worry about non-HTML tags (such as PHP), it uses the built in [crypto](https://nodejs.org/api/crypto.html) module, and it updates the HTML tags that it finds as it goes. It also allows for multiple algorithms to be specified, which is pretty cool.
+
+Tags will be ignored for the following reasons...
+* It already has an "integrity" attribute
+* It has an absolute reference (eg. starting http(s)://)
+* It's so mangled that the file path cannot be determined
 
 Status
 ======
 
-[![NPM Version](http://img.shields.io/npm/v/gulp-sri-php.svg?style=flat)](https://www.npmjs.org/package/gulp-sri-php) [![Stability](https://img.shields.io/badge/stability-experimental-orange.svg?style=flat)](https://github.com/riklewis/gulp-sri-php) [![Dependencies](http://img.shields.io/david/riklewis/gulp-sri-php.svg?style=flat)](https://david-dm.org/riklewis/gulp-sri-php) [![Development Dependencies](http://img.shields.io/david/dev/riklewis/gulp-sri-php.svg?style=flat)](https://david-dm.org/riklewis/gulp-sri-php?type=dev) [![Build Status](http://img.shields.io/travis/riklewis/gulp-sri-php.svg?style=flat)](https://travis-ci.org/riklewis/gulp-sri-php)
+[![NPM Version](http://img.shields.io/npm/v/gulp-sri-php.svg?style=flat)](https://www.npmjs.org/package/gulp-sri-php) [![Stability](https://img.shields.io/badge/stability-stable-brightgreen.svg?style=flat)](https://github.com/riklewis/gulp-sri-php) [![Dependencies](http://img.shields.io/david/riklewis/gulp-sri-php.svg?style=flat)](https://david-dm.org/riklewis/gulp-sri-php) [![Development Dependencies](http://img.shields.io/david/dev/riklewis/gulp-sri-php.svg?style=flat)](https://david-dm.org/riklewis/gulp-sri-php?type=dev) [![Build Status](http://img.shields.io/travis/riklewis/gulp-sri-php.svg?style=flat)](https://travis-ci.org/riklewis/gulp-sri-php)
 [![Coverage Status](http://img.shields.io/coveralls/riklewis/gulp-sri-php.svg?style=flat)](https://coveralls.io/r/riklewis/gulp-sri-php?branch=master) [![Gratipay](https://img.shields.io/gratipay/project/gulp-sri-php.svg)](https://gratipay.com/gulp-sri-php/)
 
 Requirements
@@ -81,4 +88,5 @@ Examples
 Configuration options
 ==========
 
+* **algorithm**: The hashing algorithm(s) you want to use (string or array - default: ["sha256","sha384","sha512"])
 * **verbose**: Will output additional messages to the console (boolean - default: false)
--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 var fs = require("fs");
-var util = require("gulp-util");
 var path = require("path");
-var through = require("through2");
 var crypto = require("crypto");
+var util = require("gulp-util");
+var through = require("through2");
+var unquote = require("unquote");
+var isarray = require("is-arrayish");
 var PluginError = util.PluginError;
 var options = {};
 var cacheArray = [];
-var version = "0.0.1"; /* must match package.json file */
+var algorithms = ["sha256","sha384","sha512"];
+var version = "1.0.0"; /* must match package.json file */
 
 function gulpSriPhp(opts) {
   options = opts;
   if(!options || options.toString()!=="[object Object]") {
     options = {};
   }
+  if(options.algorithm) {
+    if(typeof(options.algorithm)==="string") {
+      options.algorithm = [options.algorithm];
+    }
+    if(isarray(options.algorithm) && options.algorithm.length) {
+      var len = options.algorithm.length;
+      for(var i=0;i<len;i++) {
+        if(algorithms.indexOf(options.algorithm[i])<0) {
+          throw new PluginError("gulp-sri-php","Sorry, algorithm '"+options.algorithm[i]+"' is not supported. Options are: "+algorithms.join("/"));
+        }
+      }
+    }
+    else {
+      throw new PluginError("gulp-sri-php","Sorry, algorithm '"+options.algorithm+"' is not supported. Options are: "+algorithms.join("/"));
+    }
+  }
+  options.algorithm = options.algorithm || ["sha256","sha384","sha512"];
   cacheArray = []; //reset cache
   var stream = through.obj(function(file,enc,cb) {
     if(file.isNull()) {
@@ -25,7 +45,8 @@ function gulpSriPhp(opts) {
     if(options.verbose) {
       console.log("[gulp-sri-php@"+version+"] processing "+file.relative+"...");
     }
-    var cont = processStylesheets(file.contents.toString());
+    options.base = file.base || file.basename;
+    var cont = processScripts(processStylesheets(file.contents.toString()));
     file.contents = new Buffer(cont);
     this.push(file);
     return cb();
@@ -34,44 +55,111 @@ function gulpSriPhp(opts) {
 }
 
 function processStylesheets(orig) {
-  var regex = /<link.*rel=('|"){0,1}stylesheet('|"){0,1}.*>/gmi;
+  var regex = /<link.*rel=['"]{0,1}stylesheet['"]{0,1}.*>/gmi;
   var cont = orig;
   var res = null;
   while((res = regex.exec(orig))) {
     var text = res[0];
-    var name = extractFilename(text,"href"); //can I do this in the RegEx?
-    var path = calculatePath(name);
-    var hash = calculateHash(path);
-    //todo
+    var attr = extractAttributes(text);
+    if(attr.href) {
+      if(!attr.integrity) {
+        var path = calculatePath(attr.href);
+        if(path) {
+          var hash = calculateHash(path);
+          if(hash) {
+            attr.integrity = hash;
+            cont = rewriteTag(cont,text,attr);
+            console.log("[gulp-sri-php@"+version+"] hashed: "+attr.href+" ("+hash+")");
+          }
+        }
+      }
+    }
   }
   return cont;
 }
 
 function processScripts(orig) {
-  var regex = /<script.*src=['"]{0,1}([^'" ]*).*>.*<\/script>/gmi;
+  var regex = /<script.*src=['"]{0,1}([^'" ]*).*>/gmi;
   var cont = orig;
   var res = null;
   while((res = regex.exec(orig))) {
     var text = res[0];
-    var name = res[1]; //no need for extractFilename here
-    var path = calculatePath(name);
-    var hash = calculateHash(path);
-    //todo
+    var attr = extractAttributes(text);
+    if(attr.src) {
+      if(!attr.integrity) {
+        var path = calculatePath(attr.src);
+        if(path) {
+          var hash = calculateHash(path);
+          if(hash) {
+            attr.integrity = hash;
+            cont = rewriteTag(cont,text,attr);
+            console.log("[gulp-sri-php@"+version+"] hashed: "+attr.src+" ("+hash+")");
+          }
+        }
+      }
+    }
   }
   return cont;
 }
 
-function extractFilename(text,attr) {
-  //todo
+function rewriteTag(cont,text,attr) {
+  var add = " integrity=\""+attr.integrity+"\"";
+  if(!attr.crossorigin) {
+    add += " crossorigin=\"anonymous\"";
+  }
+  var tag = text.replace(/\s{0,}\/{0,1}>/,add+">");
+  return cont.replace(text,tag);
+}
+function extractAttributes(text) {
+  var att = [];
+  var arr = text.substr(1,text.length-2).split(" ");
+  var len = arr.length;
+  for(var i=1;i<len;i++) {
+    var itm = arr[i];
+    var pos = itm.indexOf("=");
+    if(pos>-1) {
+      var nam = itm.substr(0,pos).toLowerCase();
+      var val = unquote(itm.substr(pos+1));
+      att[nam] = val;
+    }
+    else {
+      itm = itm.toLowerCase();
+      att[itm] = itm;
+    }
+  }
+  return att;
 }
 
-function calculatePath(name) {
-  //todo
+function calculatePath(fnam) {
+  if(fnam.substr(0,8)==="https://" || fnam.substr(0,7)==="http://" || fnam.substr(0,2)==="//") {
+    return ""; //ignore external links
+  }
+  if(fnam.indexOf("?")>-1) {
+    fnam = fnam.substr(0,fnam.indexOf("?")); //remove query string (most likely a cache busting hash)
+  }
+  return path.normalize(path.join(options.base,fnam));
 }
 
 function calculateHash(path) {
-  var file = fs.readFileSync(path);
-  return crypto.createHash(options.algorithm).update(file).digest("base64");
+  if(!cacheArray[path]) {
+    var hash = "";
+    try {
+      var file = fs.readFileSync(path);
+      var len = options.algorithm.length;
+      for(var i=0;i<len;i++) {
+        hash += " "+options.algorithm[i]+"-"+crypto.createHash(options.algorithm[i]).update(file).digest("base64");
+      }
+      hash = hash.substr(1);
+    }
+    catch(e) {
+      hash = "";
+      if(options.verbose) {
+        console.log("[gulp-sri-php@"+version+"] failed: "+path);
+      }
+    }
+    cacheArray[path] = hash;
+  }
+  return cacheArray[path];
 }
 
 module.exports = gulpSriPhp;
